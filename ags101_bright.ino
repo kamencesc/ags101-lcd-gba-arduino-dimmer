0ed37f7127101b20fa85184d595d94c9cf65c528
Francesc Bofill <kamencesc@gmail.com>
2019-06-07 02:13:03 +0200

    Add files via upload
    Bright function implemented, defined in comments pins for Attiny85.
    
    Not tested yet.

--- ags101_bright.ino
+++ ags101_bright.ino
@@ -1,3 +1,14 @@
+//--------------------------------------------------------------------
+// LCD Bright Dimmer for a AGS-101 LCD
+// in a GBA bakclight MOD
+// 
+// Code by: Kamencesc
+// 
+// Site: http://www.kamencesc.com/
+// Git: https://github.com/kamencesc/ags101-lcd-gba-arduino-dimmer/
+// 31-01-2019
+//--------------------------------------------------------------------
+
 #include <EEPROM.h>
 
 int eeAdress = 0;     //address to save last bright
@@ -8,14 +19,19 @@ int bright = 200;
 #define MIN_BRIGHT 0
 #define DEFAULT_BRIGHT 200
 
-#define BLINK_TIMES 4
-#define BLINK_WAIT 200
+#define BTN_START 14  //A0
+#define BTN_A 15      //A1
+#define BTN_B 16      //A2
+#define bPin 11       //11 PWM
 
-#define BTN_START 9           //  D9
-#define BTN_A 10              // D10
-#define BTN_B 11              // D11
+/* ATTINY
+#define BTN_START 4
+#define BTN_A 3
+#define BTN_B 2
+#define bPin 0        //PWM
+*/
 
-#define OUTPUT_PWM 2          //  A2
+#define BLINK_TIMES 3
 
 bool btn_start_press = false;
 bool btn_a_press = false;
@@ -24,13 +40,6 @@ bool btn_b_press = false;
 bool btn_start_last = false;
 bool btn_a_last = false;
 bool btn_b_last = false;
-
-void ledBlinking(int delay_time) {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(delay_time);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(delay_time);
-}
 
 int getBright(int count, int actual_bright) {
   int value = actual_bright + count;
@@ -41,48 +50,34 @@ int getBright(int count, int actual_bright) {
 
 void setBright(int value) {
   //set bright
-  if (value >= 250 && value <= 0)   {
-    errorBlinking();
-    value = DEFAULT_BRIGHT;
-  }
-  analogWrite(OUTPUT_PWM, value);
-}
-
-void errorBlinking() {
-  for (int n = 0; n<= 5; n++) {
-    for (int i = 0; i<= 2; i++) {
-      ledBlinking(100);
-    }
-    delay(300);
-  }
+  analogWrite(bPin, bright);
 }
 
 void setup() {
-  // Initializing PINs
+  // put your setup code here, to run once:
   pinMode(BTN_START, INPUT_PULLUP);     //START
   pinMode(BTN_A, INPUT_PULLUP);         //A
   pinMode(BTN_B, INPUT_PULLUP);         //B
 
-  pinMode(OUTPUT_PWM, OUTPUT);          //output pin
+  //power off led
+  digitalWrite(LED_BUILTIN, LOW);
 
-  //read EEPORM for last bright value
   int eeBright;  
   EEPROM.get(eeAdress,eeBright);
   if (eeBright >= 250 && eeBright <= 0) { //invalid value
-    //if not a valid value, then set a default value
-    //and update to the EEPROM
+    //write the default value
     EEPROM.update(eeAdress, DEFAULT_BRIGHT);
     bright = DEFAULT_BRIGHT;
   } else {
     //put the read value to bright variable
     bright = eeBright;
   }
-  //set bright
   setBright(bright);
 }
 
 void loop() {
-  //read PINs
+  // put your main code here, to run repeatedly:
+
   btn_start_press = !digitalRead(BTN_START);
   btn_a_press = !digitalRead(BTN_A);
   btn_b_press = !digitalRead(BTN_B);
@@ -90,15 +85,18 @@ void loop() {
   if (btn_start_press) {        //START PRESSED
     if (btn_a_press && btn_b_press ) {
       if (btn_a_press != btn_a_last && btn_b_press != btn_b_last) {
-        //START+A+B = reset
+        //reset
         bright = DEFAULT_BRIGHT;
         setBright(bright);
-        //blinking led a result of reset
-        for (int i = 0; i <= BLINK_TIMES; i++) {
-          ledBlinking(BLINK_WAIT);
-        }
         //update EEPROM if it's necessary
         EEPROM.update(eeAdress,bright);
+        //blink led as reset signal
+        for (int i = 0; i++; i <= BLINK_TIMES ) {
+          delay(200);
+          digitalWrite(LED_BUILTIN, HIGH);
+          delay(200);
+          digitalWrite(LED_BUILTIN, LOW);
+        }
       }
     }
     else if (btn_a_press) {           //START + A
@@ -128,4 +126,6 @@ void loop() {
   btn_start_last = btn_start_press;
   btn_a_last = btn_a_press;
   btn_b_last = btn_b_press;
+  //add delay
+  delay(500);
 }
